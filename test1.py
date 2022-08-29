b51ca6e0b48ea1a70f92dc04996437a2f1a24f52
DESKTOP-2RSC2IR\Heybye <e.shestakov@gamemavericks.com>
2022-08-30 02:16:50 +0700

    First step

--- test1.py
+++ test1.py
@@ -1,1 +1,10 @@
-print('hello')
+first_question = input('In winter and in summer has same colour : ')
+if first_question == 'Tree':
+    print('Succseed')
+elif first_question == 'tree':
+        print('Succseed')
+else:
+            print('Wasted')
+
+
+
